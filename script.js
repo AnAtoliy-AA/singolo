e846225f1d3cadbb7ef33af72d1dd059beeb8770
AnAtoliy_AK <aliaksandrau89@gmail.com>
2020-03-14 01:38:16 +0200

    feat: contact form

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
-const MENU = document.getElementById('menu');
+const NAVBAR = document.getElementById('navbar');
+const PICMENU = document.getElementById('picture-menu');
+const PICTURES = document.getElementById('pictures');
+const SLIDER = document.getElementById('slider');
 const BUTTON = document.getElementById('btn');
 const CLOSE_BUTTON = document.getElementById('close-btn');
 
 
-MENU.addEventListener('click', (event)=>{
-  MENU.querySelectorAll('a').forEach(el => el.classList.remove('navigation__item_selected'));
+NAVBAR.addEventListener('click', (event)=>{
+  NAVBAR.querySelectorAll('a').forEach(el => el.classList.remove('navigation__item_selected'));
   event.target.classList.add('navigation__item_selected');
 });
 
+/*carousel*/
+
+SLIDER.addEventListener('click', ()=>{
+ /* SLIDER.querySelectorAll('a').forEach(el => el.classList.remove('navigation__item_selected'));*/
+  SLIDER.style.backgroundImage = 'url(assets/layer4.png)';
+});
+
+
+PICMENU.addEventListener('click', (event)=>{
+  PICMENU.querySelectorAll('button').forEach(el => el.classList.remove('button__selected'));
+  event.target.classList.add('button__selected');
+});
+
+PICTURES.addEventListener('click', (event)=>{
+  PICTURES.querySelectorAll('img').forEach(el => el.classList.remove('img__selected'));
+  event.target.classList.add('img__selected');
+});
+
 BUTTON.addEventListener('click', ()=>{
-  const subject = document.getElementById('subject').value.toString;
-  document.getElementById('result').innerText = subject;
+  const subject = document.getElementById('subject').value.toString();
+  const describe = document.getElementById('describe').value.toString();
+  if (document.getElementById('subject').value.toString().length == 0) {
+    document.getElementById('result1').innerText = ('Without topic');
+  }
+  else  document.getElementById('result1').innerText = ('Topic:' + subject);
+ if (document.getElementById('describe').value.toString().length == 0){
+  document.getElementById('result2').innerText = ('Without describe');
+ }
+  else document.getElementById('result2').innerText = ('Describe:' + describe);
   document.getElementById('message-block').classList.remove('hiden');
 });
 CLOSE_BUTTON.addEventListener('click', ()=>{
-  document.getElementById('result').innerText = '';
+  document.getElementById('result1').innerText = '';
+  document.getElementById('result2').innerText = '';
   document.getElementById('message-block').classList.add('hiden');
 });
+
+
+
 
 
 
