351f84108b67711d74e6762fe328d8fb0a9d8be4
AnAtoliy_AK <aliaksandrau89@gmail.com>
2020-03-13 18:03:56 +0300

    feat: add some js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const MENU = document.getElementById('menu');
-
+const BUTTON = document.getElementById('btn');
+const CLOSE_BUTTON = document.getElementById('close-btn');
 
 
 MENU.addEventListener('click', (event)=>{
@@ -7,7 +8,15 @@ MENU.addEventListener('click', (event)=>{
   event.target.classList.add('navigation__item_selected');
 });
 
-
+BUTTON.addEventListener('click', ()=>{
+  const subject = document.getElementById('subject').value.toString;
+  document.getElementById('result').innerText = subject;
+  document.getElementById('message-block').classList.remove('hiden');
+});
+CLOSE_BUTTON.addEventListener('click', ()=>{
+  document.getElementById('result').innerText = '';
+  document.getElementById('message-block').classList.add('hiden');
+});
 
 
 
